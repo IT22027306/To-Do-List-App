386429039926dd0364647122e8422129264cf1b1
IT22027306 <121513753+IT22027306@users.noreply.github.com>
2024-05-10 23:08:38 +0530

    Implement all data display function

--- app/src/main/java/com/example/to_do_list/MainActivity.kt
+++ app/src/main/java/com/example/to_do_list/MainActivity.kt
@@ -6,15 +6,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.to_do_list.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+    private lateinit var db : NotesDatabaseHelper
+    private lateinit var notesAdopter: NotesAdopter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        db = NotesDatabaseHelper(this)
+        notesAdopter = NotesAdopter(db.getAllNotes(), this)
+
+        binding.noteRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.noteRecyclerView.adapter = notesAdopter
 
         binding.addButton.setOnClickListener {
             val intent = Intent(this, AddNoteActivity::class.java)
@@ -22,4 +31,13 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    override fun onResume() {
+        super.onResume()
+        notesAdopter.refreshData(db.getAllNotes())
+    }
+
+
+
 }
+
